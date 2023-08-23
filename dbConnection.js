d87d5a9781e257c94b1825d5c2b6d362ebcbede2
Caique <caiquealvesdesouza27@gmail.com>
2023-08-23 01:10:14 -0300

    Alterações para a conexão com o banco de dados virtualizado

--- dbConnection.js
+++ dbConnection.js
@@ -1,16 +1,17 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
 const con = mysql.createConnection({
-	host: "localhost",
-	port: 3030,
+	host: "containers-us-west-103.railway.app",
+	port: 6726,
 	user: "root",
-	password: "senha",
-	database: "planner_database",
+	password: "rLhj29ppd0MAjJtQ8dXn",
+	database: "railway",
 });
 
 con.connect((err) => {
 	if (err) {
 		console.log("Error connecting to the database");
+		console.log(err);
 	} else {
 		console.log("Succesfully connected to the database");
 	}
